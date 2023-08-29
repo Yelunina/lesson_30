14329ca2c84dc6f898ad22a77be81cefd725d28d
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-08-29 10:05:03 +0200

    init commit

---
+++ src/ait/employee/dao/Company.java
@@ -0,0 +1,23 @@
+package ait.employee.dao;
+
+import ait.employee.model.Employee;
+
+public interface Company {
+    boolean addEmployee(Employee employee);
+
+    Employee removeEmployee(int id);
+
+    Employee findEmployee(int id);
+    int quantity();
+    double totalSalary();
+    double avgSalary();
+
+    double totalSales();
+
+    void printEmployees();
+
+    Employee[] findEmployeesHoursGreaterThan(int hours);
+
+    Employee[] findEmployeeSalaryRange(int minSalary, int maxSalary);
+
+}
